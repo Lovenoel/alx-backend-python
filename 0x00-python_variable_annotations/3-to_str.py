d7ce19e874828557e887079346aa5815db76b792
lovenoel <kinglovenoel@gmail.com>
2024-10-12 04:08:45 +0300

    Tasks committed

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -6,7 +6,7 @@ Convert float to string with type annotations.
 def to_str(n: float) -> str:
     """
     Return the string representation of a float.
-    
+
     :param n: a float number
     :return: string representation of n
     """
