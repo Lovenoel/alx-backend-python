a232e389449503c7abd4b4c891d5cb1d740268f8
lovenoel <kinglovenoel@gmail.com>
2024-11-05 01:24:11 +0000

    Task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unit tests for client.GithubOrgClient"""
 
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -36,5 +36,29 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("test")
         self.assertEqual(
                 client._public_repos_url,
+                "https://api.github.com/orgs/test/repos"
+                )
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests for the GithubOrgClient class"""
+
+    @patch("client.get_json")
+    @patch(
+            "client.GithubOrgClient._public_repos_url",
+            new_callable=PropertyMock
+            )
+    def test_public_repos(
+            self,
+            mock_public_repos_url: PropertyMock,
+            mock_get_json: Mock) -> None:
+        """Test public_repos method returns the expected list of repos"""
+        mock_public_repos_url.return_value =
+        "https://api.github.com/orgs/test/repos"
+        mock_get_json.return_value = [{"name": "repo1"}, {"name": "repo2"}]
+
+        client = GithubOrgClient("test")
+        self.assertEqual(client.public_repos(), ["repo1", "repo2"])
+        mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/test/repos"
                 )
